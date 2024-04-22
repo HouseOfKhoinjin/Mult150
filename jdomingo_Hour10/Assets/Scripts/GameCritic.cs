fdbc825189f9fd40157fd8ad94953690f9d85f7d
HouseOfKhoinjin <jhd5795@gmail.com>
2024-04-22 01:32:04 -0700

    Finished Hour 10

---
+++ jdomingo_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,19 @@
+/* 
+ 
+ 1. Is the game too easy or hard?
+
+    A: it feels too easy at the beginning and too hard near the end. Once it begins, 3 of the balls just go in by itself after half a minute. After that, it gets a bit tedious trying to get the last one in, which explain more about in the 4th question
+
+ 2. What would make it easier or harder?
+
+    A: making the normal balls slower or the the goals bigger would make the game easier, while making the chaos balls bigger would make the game harder
+ 
+  3. What would give it a "wow" factor? 
+ 
+    A: In my opinion, making it so that the goal makes a little explosion before deleting itself sounds cool. Other than that, some music would be nice
+ 
+ 4. What parts of the game are fun? What parts are tedious?
+ 
+    A:  I find it fun using the chaos balls to knock the normal balls into the goal. However, the normal speeds feel a bit too slow, because I always end up missing the balls by an inch
+ 
+ */
