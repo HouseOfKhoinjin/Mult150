773d6b92ed1eed82e9b5a1b0481dce7e63cb202e
HouseOfKhoinjin <jhd5795@gmail.com>
2024-05-05 23:40:33 -0700

    Hours 14 and 16 done

---
+++ jdomingo_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+ * changed particle material to light blue 
+ * changed particle lifetime to half a second 
+ * changed shape module to box so that the particles only go up
+ * changed size over lifetime to so that the particles shrink before deleting itself
+ * made the particle color go from blue to yellow during its lifetime
+*/
